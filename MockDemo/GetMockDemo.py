09eadddee54cb04d9b8d0a725539adfe8eef858b
xuepengju <xue_pengju@163.com>
2022-02-18 00:37:07 +0800

    update some demo and add 冰墩墩

---
+++ MockDemo/GetMockDemo.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Time: 2022/2/17 11:13 上午
+# @Author: xiajian
+# @File: GetMockDemo.py
+# @description: 启动一个get请求的接口服务
+
+
+import flask
+import json
+import time
+
+server = flask.Flask(__name__)  # 创建一个服务，把当前这个python文件当做一个服务
+
+
+@server.route('/get/date_time', methods=['get'])
+# @server.route()可以将普通函数转变为服务、接口的路径、请求方式，如果不写methods则默认get方法
+def date_time():
+    """查询字符串:无，消息体：无，返回结果：SystemTime"""
+    response_data = {
+        "SystemTimeObject": {
+            "VIIDServerID": "123",
+            "TimeMode": "1",
+            "LocalTime": time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())),
+        }
+    }
+    return json.dumps(response_data, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    server_host = '127.0.0.1'
+    server_port = 5005
+    server_path = "/get/date_time"
+    print("http://" + server_host + ":" + str(server_port) + server_path)
+    server.run(debug=True, port=server_port, host=server_host)  # 指定访问端口、host
---
+++ MockDemo/PostMockDemo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time: 2022/2/17 11:13 上午
+# @Author: xiajian
+# @File: GetMockDemo.py
+# @description: 启动一个post请求的接口服务,通过参数的key返回对应擦值
+
+
+import flask
+import json
+
+server = flask.Flask(__name__)  # 创建一个服务，把当前这个python文件当做一个服务
+
+
+@server.route('/data', methods=['post'])
+def post_json():
+    if flask.request.is_json:
+        print(flask.request.json)
+        name = flask.request.json.get('name')  # 获取json请求体的第一个参数的值
+        age = flask.request.json.get('age')  # 获取json请求体的第二个参数的值
+        data = {'name': name, 'age': age}
+        # 构建响应头域和状态码
+        resp = flask.make_response(json.dumps(data, ensure_ascii=False))
+        resp.status = "888"
+        resp.content_type = "application/json"
+        resp.headers[
+            "python"] = "python flask"
+        return resp
+    else:
+        return json.dumps({'msg': '请传json格式参数'}, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    print("http://127.0.0.1:5010/data")
+    server.run(debug=True, port=5010, host='127.0.0.1')  # 指定访问端口、host
